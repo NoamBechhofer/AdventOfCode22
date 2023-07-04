e60d08aea8c8a56231074cc43929369ff93fdd7a
NoamBechhofer <noam.bechhofer@gmail.com>
2023-07-04 01:38:15 -0400

    started day 3

---
+++ day/three/rust/src/main.rs
@@ -0,0 +1,142 @@
+use std::{collections::HashSet, env, fmt::Display, iter::Sum};
+
+const NUM_COMPARTMENTS: usize = 2;
+
+struct Rucksack<T> {
+    contents: [Compartment<T>; NUM_COMPARTMENTS],
+}
+
+impl<T: Display> Display for Rucksack<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut ret = String::from("");
+        for compartment in self.contents.iter() {
+            ret += "[";
+            for item in compartment.items.iter() {
+                ret += &format!("{}, ", item);
+            }
+            ret += "]\n";
+        }
+        write!(f, "{}", ret)
+    }
+}
+
+impl<T> Default for Rucksack<T> {
+    fn default() -> Self {
+        Rucksack::new()
+    }
+}
+
+impl<T> Rucksack<T> {
+    fn new() -> Self {
+        Rucksack {
+            contents: [Compartment::new(), Compartment::new()],
+        }
+    }
+}
+
+impl From<&str> for Rucksack<char> {
+    fn from(s: &str) -> Self {
+        let mut rucksack: Rucksack<char> = Rucksack::new();
+
+        let mut s_prime = s;
+        let s_prime_len = s_prime.len();
+        assert!(s_prime_len % rucksack.contents.len() == 0);
+
+        let frac_len = s_prime_len / rucksack.contents.len();
+        for compartment in rucksack.contents.iter_mut() {
+            for _ in 0..frac_len {
+                compartment.items.push(s_prime.chars().nth(0).unwrap());
+                if s_prime.len() > 1 {
+                    s_prime = &s_prime[1..];
+                }
+            }
+        }
+        rucksack
+    }
+}
+
+struct Compartment<T> {
+    items: Vec<T>,
+}
+
+impl<T> Default for Compartment<T> {
+    fn default() -> Self {
+        Compartment::new()
+    }
+}
+
+impl<T> Compartment<T> {
+    fn new() -> Self {
+        Compartment { items: vec![] }
+    }
+}
+
+type Priority = u8;
+impl From<&Rucksack<char>> for Option<Priority> {
+    fn from(value: &Rucksack<char>) -> Self {
+        if let Some(shared_item) = find_some_shared_item(&value) {
+            return priority_from_char(shared_item);
+        } else {
+            return None;
+        }
+    }
+}
+
+impl<'a> Sum<&'a Rucksack<char>> for u32 {
+    fn sum<I: Iterator<Item = &'a Rucksack<char>>>(iter: I) -> Self {
+        iter.map(|r| {
+            return match Option::<Priority>::from(r) {
+                Some(priority) => priority as u32,
+                None => 0,
+            };
+        })
+        .sum()
+    }
+}
+
+fn priority_from_char(c: char) -> Option<Priority> {
+    if c < 'A' || (c > 'Z' && c < 'a') || c > 'z' {
+        None
+    } else if c >= 'a' {
+        Some(c as u8 - b'a' + 1)
+    } else {
+        Some(c as u8 - b'A' + 1 + 26)
+    }
+}
+
+fn find_some_shared_item(rucksack: &Rucksack<char>) -> Option<char> {
+    let mut map: HashSet<char> = HashSet::new();
+    for compartment in rucksack.contents.iter() {
+        for item in compartment.items.iter() {
+            if map.contains(item) {
+                return Some(*item);
+            }
+        }
+        map.extend(compartment.items.iter());
+    }
+    return None;
+}
+
+fn main() {
+    let argv: Vec<String> = env::args().collect();
+    if argv.len() != 2 {
+        panic!("Usage: {} <input file>", argv[0]);
+    }
+
+    let filepath = &argv[1];
+    let contents = std::fs::read_to_string(filepath).expect("Failed to read file");
+
+    let mut rucksacks: Vec<Rucksack<char>> = vec![];
+    for line in contents.lines() {
+        let curr = Rucksack::from(line);
+        // println!(
+        //     "Rucksack:\n{}Duplicate: {}",
+        //     curr,
+        //     find_some_shared_item(&curr).unwrap()
+        // );
+        rucksacks.push(curr);
+    }
+
+    let total_priority: u32 = rucksacks.iter().sum();
+    println!("Total priority: {}", total_priority);
+}
